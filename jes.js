26161017d101cd071af5cafdc29be8cdd08f65c5
paulVer777 <pawel.gitara@onet.eu>
2018-04-17 09:31:36 +0200

    reduce

--- jes.js
+++ jes.js
@@ -1,29 +1,38 @@
+var Cat = function (name, legs, sound) {
+    this.name = name;
+    this.legs = legs;
+    this.sound = sound;
+};
+Cat.prototype.makesound = function () {
+
+    console.log(this.sound);
+};
 
 
-function show()
-{
-
-    console.log(this);
-
-
-}
+var mruczek = new Cat("Bonifacy", 4, "miauu");
+var mruczek1 = new Cat("Bonifacy", 4, "miauu");
+var mruczek2 = new Cat("Wiertarka", 4, "miauu");
+var mruczek3 = new Cat("Bonifacy", 4, "miauu");
+var mruczek4 = new Cat("Bonifacy", 4, "miauu");
 
 
+var names = ["bartek", "magda", "paweł", "jan", "mateusz", "ala", "ola"];
 
-show();
+names.reduce(function (reduced, value, index, array) {
+
+    if (value.length < 4) {
+
+        reduced.shorterThan += 1;
+
+    }
+    else {
+
+        reduced.biggerThan += 1;
+
+    }
+
+    return reduced;
 
 
-
-
-var dino={};
-
-
-dino.logThis=show;
-
-
-dino.logThis();
-
-
-
-
+}, {biggerThan: 0, shorterThan: 0});
 
