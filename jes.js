8b397d8af3709a3d312ce9933710160e3dfb997f
paulVer777 <pawel.gitara@onet.eu>
2018-04-17 15:40:30 +0200

    oop

--- jes.js
+++ jes.js
@@ -1,33 +1,66 @@
-var Cat = function (name, legs, sound) {
+var Cat = function (name, sound) {
     this.name = name;
-    this.legs = legs;
+    this.speak =false;
     this.sound = sound;
 };
-Cat.prototype.makesound = function () {
+
+Cat.prototype.makeSound = function () {
 
     console.log(this.sound);
 };
 
 
-var mruczek = new Cat("Bonifacy", 4, "miauu");
-var mruczek1 = new Cat("Bonifacy", 4, "miauu");
-var mruczek2 = new Cat("Wiertarka", 4, "miauu");
-var mruczek3 = new Cat("Bonifacy", 4, "miauu");
-var mruczek4 = new Cat("Bonifacy", 4, "miauu");
+var mruczek = new Cat("Bonifacy","miauu");
+var mruczek1 = new Cat("Bonifacy","Grrrrrrrrrrrrrr");
+var mruczek2 = new Cat("Wiertarka","miauu");
+var mruczek3 = new Cat("Bonifacy","miauu");
+var mruczek4 = new Cat("Bonifacy","miauu");
+
+
+mruczek1.makeSound();
 
 
 
 
-var dog={
-    sound:"pramiauu",
-    makesound: function (){console.log(this.sound)}
+
+var dog = {
+    sound: "hau",
+    makesound: function () {
+        console.log(this.sound)
+    }
 };
 
 
-var dog1=Object.create(dog);
+var dog1 = Object.create(dog);
 
-dog1.sound="meoow";
+dog1.sound = "meoow";
 
-dog1.makesound();
+//dog1.makesound();
+
+var sheep = {
+
+    eyes: 4,
+    legs: 2
+
+};
+
+var cow = {
+
+    tail: 1,
+    color: "white"
+
+
+};
+
+var spider = {
+
+    eyes: 20,
+    name: "Sssh"
+
+
+};
+
+
+var mutant = Object.assign({}, sheep, cow, spider);
 
 
